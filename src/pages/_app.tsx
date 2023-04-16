3525c454b2f782c68b2cd42415c50e4158f8d84e
개인문타리 <muntrock@gmail.com>
2023-04-17 01:16:14 +0900

    로컬스토리지 테마 값 활용 및 FOUC 깜빡임 UX 개선

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,11 +5,33 @@ import { SessionProvider } from "next-auth/react";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import { useEffect, useState } from "react";
+import { getLocalStorageItem } from "~/utils/localStorage";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
+  const [isDark, setDark] = useState<boolean | null>(null)
+  
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const theme = getLocalStorageItem('theme')
+      if (theme === 'dark' || (!theme && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+        document.documentElement.classList.add('dark')
+        setDark(true)
+      } else {
+        document.documentElement.classList.remove('dark')
+        setDark(false)
+      }
+    }
+  }, [])
+
+  // NOTE 페이지 새로고침시 깜빡임 UX 이슈 개선 (feat isDark, setDark)
+  if (isDark === null){
+    return <></>
+  }
+
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
